362943ec2e3079931f642a85758b33b967268311
Nicolas <nicolas@macbook-pro-de-nicolas.home>
2017-06-16 18:23:00 +0200

    add model Product

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def home
+    @today = Date.today
+  end
+  def team
+    @members = ["Nico", "Romain", "Julie"]
+  end
+  def join_us
+    render plain: "Send us a mail !"
   end
 end
