870e69890a178cc3e2b51f07207981661faa27f5
Nicolas <nicolas@macbook-pro-de-nicolas.home>
2017-06-17 12:21:01 +0200

    implement new and create actions on product + create seed for db

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,9 +9,16 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -22,5 +29,11 @@ class ProductsController < ApplicationController
 
   def destroy
   end
-  
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :url)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+Product.create!(name: "Google", url: "https://google.com", tagline: "Don't be evil")
+Product.create!(name: "And Co", url: "https://www.and.co/",
+                               tagline: "Invoicing, Expenses, Contracts & Payments for your business")
+Product.create!(name: "Bonsai", url: "https://www.hellobonsai.com", 
+                               tagline: "Free bulletproof contracts for designers & developers")
