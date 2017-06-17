08ba6170563417999e7d6403f7b466c188651b83
Nicolas <nicolas@macbook-pro-de-nicolas.home>
2017-06-17 13:56:59 +0200

    add update and destroy actions + code refacto

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,13 @@
 class ProductsController < ApplicationController
 
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
+
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
+    # @product = Product.find(params[:id]) i.e find_product
   end
 
   def new
@@ -22,18 +24,32 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    # @product = Product.find(params[:id]) i.e find_product
   end
 
   def update
+    # @product = Product.find(params[:id]) i.e find_product
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    # @product = Product.find(params[:id]) i.e find_product
+    @product.destroy
+    redirect_to products_path
   end
 
   private
 
   def product_params
     params.require(:product).permit(:name, :url)
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
   end
 
 end
