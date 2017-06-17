45c4cf513577c54bcc7e9b698b77ac6169c34f8e
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2017-06-17 16:33:57 +0200

    complete front-end

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -45,7 +45,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :url)
+    params.require(:product).permit(:name, :url, :tagline)
   end
 
   def find_product
